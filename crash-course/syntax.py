ef32980da1f29242448bbdaec1bc6f5b7c5f3ae6
akibnayan <akibnayan182@gmail.com>
2025-05-11 11:50:40 +0600

    python syntax

---
+++ crash-course/syntax.py
@@ -0,0 +1,11 @@
+# Variable
+# for
+# while
+# do while
+# condition -> if | switch statement
+# OOP
+
+if 20 > 10:
+    print("20 is greater than 10")
+else:
+    print("20 is not greater than 10")
